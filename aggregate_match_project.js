942f67deba25bf67e43b96fa9e243eb4e7c167ba
Christopher Legali <christo.lagali@gmail.com>
2019-02-22 16:34:25 -0800

    Adding Concat Feature to the Project aggregation.

--- aggregate_match_project.js
+++ aggregate_match_project.js
@@ -3,6 +3,6 @@ var value = db.employees.aggregate(
 	{
 		"$match":{"team":"Orange"}},
 	{
-		"$project":{"_id":0,"team":1,"age":1,"gender":1}
+		"$project":{"_id":0,"Name":{"$concat":["$first_name","-","$last_name"]},"team":1,"age":1,"gender":1}
 	}
 ])
