a091d0753c027748b19c6142a944f09057d49a80
christo.lagali <christo.lagali@gmail.com>
2019-02-23 15:41:41 -0800

    Using unwind to unpack the genres array.

--- agg_match_multiple_unwind.js
+++ agg_match_multiple_unwind.js
@@ -1,5 +1,5 @@
 var value = db.movies.aggregate([
 	{"$match":{"year":1913}},
 	{"$unwind":"$genres"},
-	{"$project":{"_id":0,"title":1,"type":1,"genres":1}}
+	{"$project":{"_id":0,"title":1,"genres":1,"cast":1}}
 ]);
