800e3bcd5398f344908fad6a3f56ec6c24a0dcdd
christo.lagali <christo.lagali@gmail.com>
2019-02-23 10:31:40 -0800

    Adding fetch stage to limit the no of rows.

--- aggregate_match_project.js
+++ aggregate_match_project.js
@@ -1,7 +1,11 @@
 var value = db.employees.aggregate(
 [
 	{
-		"$match":{"team":"Orange"}},
+		"$match":{"team":"Orange"}
+	},
+	{
+		"$limit":5
+	},
 	{
 		"$project":{"_id":0,"Name":{"$concat":["$first_name","-","$last_name"]},"team":1,"age":1,"gender":1}
 	}
