52f184f043817c317ab3b826ae68d481fe515ffe
christo.lagali <christo.lagali@gmail.com>
2019-02-23 16:08:50 -0800

    Adding multiple unwinds to filter 2 arrays.

--- agg_match_multiple_unwind.js
+++ agg_match_multiple_unwind.js
@@ -1,5 +1,8 @@
 var value = db.movies.aggregate([
 	{"$match":{"year":1913}},
 	{"$unwind":"$genres"},
-	{"$project":{"_id":0,"title":1,"genres":1,"cast":1}}
+	{"$match":{"$or":[{"genres":"Drama"},{"genres":"Crime"}]}},
+	{"$project":{"_id":0,"title":1,"genres":1,"cast":1}},
+	{"$unwind":"$cast"},
+	{"$match":{"$or":[{"cast":"Edmund Breon "},{"cast":"Georges Melchior"}]}}
 ]);
